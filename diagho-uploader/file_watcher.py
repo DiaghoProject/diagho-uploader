163ae4efb0a82e906a91698e3fb8be15f3356a18
bnouyou <benedicte.nouyou@gmail.com>
2024-08-01 11:05:34 +0200

    Remove processed file

--- diagho-uploader/file_watcher.py
+++ diagho-uploader/file_watcher.py
@@ -52,7 +52,7 @@ class MyHandler(FileSystemEventHandler):
                 
                 time.sleep(3)
                 
-                # self.remove_file(file_path)
+                self.remove_file(file_path)
                 
             else:
                 print(f"Ignored file: {file_path}")
