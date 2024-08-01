6880cc659921f302a7459e11d0ffc5a58d669562
bnouyou <benedicte.nouyou@gmail.com>
2024-08-01 11:47:06 +0200

    Add main.sh

---
+++ diagho-inputs/main.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+source /home/${USER}/miniconda3/etc/profile.d/conda.sh
+
+
+# Arguments:
+BATCHID="EXOME-71"
+INPUT_FILE="${DIR_DIAGHO}/cytogen/input_files/${BATCHID}/diagho_${BATCHID}.json"
+OUTPUT_DIR="${DIR_DIAGHO}/cytogen/input_files/${BATCHID}/"
+OUTPUT_PREFIX="${BATCHID}"
+
+DIR_VCFS="${DIR_DIAGHO}/DIAGHO-UPLOADER/input_biofiles"
+TARGET_DIRECTORY="${DIR_DIAGHO}/DIAGHO-UPLOADER/input_data"
+
+
+
+
+# Script :
+conda activate python-3.10
+
+python /ngs/datagen/cytogen/SCRIPTS/package_diagho_input_files/diagho_input_files/diagho_tsv2json.py \
+    --input_file $INPUT_FILE \
+    --input_type "json" \
+    --output_directory $OUTPUT_DIR \
+    --output_prefix $OUTPUT_PREFIX \
+    --vcfs_directory $DIR_VCFS \
+    --target_directory $TARGET_DIRECTORY
