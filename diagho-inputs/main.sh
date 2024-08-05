98900b91572dcdf012c03c421b020b893012892c
bnouyou <benedicte.nouyou@gmail.com>
2024-08-05 14:44:42 +0200

    main script

--- diagho-inputs/main.sh
+++ diagho-inputs/main.sh
@@ -3,22 +3,22 @@ source /home/${USER}/miniconda3/etc/profile.d/conda.sh
 source diagho-inputs/config.sh
 
 # Arguments:
-# BATCHID="EXOME-78"
-# INPUT_FILE="${DIR_DIAGHO}/cytogen/input_files/${BATCHID}/diagho_${BATCHID}.json"
-# OUTPUT_DIR="${DIR_DIAGHO}/cytogen/input_files/${BATCHID}/"
-# OUTPUT_PREFIX="${BATCHID}"
-
-# DIR_VCFS="${DIR_DIAGHO}/DIAGHO-UPLOADER/input_biofiles"
-# TARGET_DIRECTORY="${DIR_DIAGHO}/DIAGHO-UPLOADER/input_data"
-
-# BATCHID="EXOME-71"
-BATCHID="TEST"
-# INPUT_FILE="/ngs/datagen/diagho/cytogen/input_files/${BATCHID}/diagho_${BATCHID}.json"
-INPUT_FILE="/ngs/datagen/diagho/cytogen/input_files/TEST/test2.tsv"
-DIR_VCFS="/ngs/datagen/diagho/DIAGHO-UPLOADER/input_biofiles"
-OUTPUT_DIR="/ngs/datagen/diagho/cytogen/input_files/${BATCHID}/"
+BATCHID="EXOME-78"
+INPUT_FILE="${DIR_DIAGHO}/cytogen/input_files/${BATCHID}/diagho_${BATCHID}.json"
+OUTPUT_DIR="${DIR_DIAGHO}/cytogen/input_files/${BATCHID}/"
 OUTPUT_PREFIX="${BATCHID}"
-TARGET_DIRECTORY="/ngs/datagen/diagho/DIAGHO-UPLOADER/input_data"
+
+DIR_VCFS="${DIR_DIAGHO}/DIAGHO-UPLOADER/input_biofiles"
+TARGET_DIRECTORY="${DIR_DIAGHO}/DIAGHO-UPLOADER/input_data"
+
+
+# BATCHID="TEST"
+# INPUT_FILE="/ngs/datagen/diagho/cytogen/input_files/${BATCHID}/diagho_${BATCHID}.json"
+# INPUT_FILE="/ngs/datagen/diagho/cytogen/input_files/TEST/test2.tsv"
+# DIR_VCFS="/ngs/datagen/diagho/DIAGHO-UPLOADER/input_biofiles"
+# OUTPUT_DIR="/ngs/datagen/diagho/cytogen/input_files/${BATCHID}/"
+# OUTPUT_PREFIX="${BATCHID}"s
+# TARGET_DIRECTORY="/ngs/datagen/diagho/DIAGHO-UPLOADER/input_data"
 
 
 # Script :
