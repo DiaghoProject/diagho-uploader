b7827dcedcd69a7989a7902a840ae2294d2f5f76
bnouyou <benedicte.nouyou@gmail.com>
2024-09-20 15:55:34 +0200

    Changes for get loading_status

--- diagho-uploader/process_file.py
+++ diagho-uploader/process_file.py
@@ -97,23 +97,27 @@ def get_files_infos(json_input):
     """
     try:
         input_data = pd.read_json(json_input)
+        
+        # Récup des infos dans un dict
+        dict_files = {}
+        for file in input_data['files']:
+            filename = file['filename']
+            checksum = file['checksum']
+            samples = file['samples']
+            persons = []
+            for sample in samples:
+                persons.append(sample["person"])
+            dict_files[filename] = {
+                "checksum" : checksum,
+                "persons" : persons
+            }
+        return dict_files
+
     except ValueError as e:
         print(f"Erreur lors de la lecture du JSON: {e}")
         return {}
     
-    dict_files = {}
-    for file in input_data['file']:
-        filename = file['filename']
-        checksum = file['checksum']
-        samples = file['samples']
-        persons = []
-        for sample in samples:
-            persons.append(sample["person"])
-        dict_files[filename] = {
-            "checksum" : checksum,
-            "persons" : persons
-        }
-    return dict_files
+    
    
 
 # Fonction principale : process JSON file
@@ -132,38 +136,59 @@ def diagho_process_file(file, config):
     logging.info(f"Processing file: {file}")
     
     # Configuration et URLs
-    config_file = "config/config.yaml" # TODO #9 #8 mettre à l'exterieur de la fonction
+    print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("1. Chargement de la config")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+    pretty_print_json_string(config)
     recipients = config['emails']['recipients']
     path_biofiles = config['input_biofiles']
     get_biofile_max_retries = config['check_biofile']['max_retries']
     get_biofile_delay = config['check_biofile']['delay']
-    # url_diagho_api_healthcheck = config['diagho_api']['healthcheck']
     
     # Vérification de l'API Diagho
-    if not diagho_api_test(config_file):
+    print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("2. Test connexion API")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+    if not diagho_api_test(config):
         content = "Failed to connect to Diagho API. Exit."
         send_mail_alert(recipients, content)
         logging.error(content)
         return
     
     # Connexion à l'API
-    diagho_api_login(config_file)
-    access_token = get_access_token('tokens.json')
+    print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("3. Login API")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+    # Login
+    diagho_api_login(config)
+    
+    # Récup le token
+    access_token = get_access_token(config, 'tokens.json', )
+    print(f"Access token: {access_token}")
     logging.info(f"Access token: {access_token}")
     
     # Traitement du fichier
+    print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("4. Test format du fichier : TSV ou JSON")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
     if file.endswith('.tsv'):
+        print(f"TSV file detected. Parsing TSV to JSON for file: {file}")
         logging.info(f"TSV file detected. Parsing TSV to JSON for file: {file}")
         # TODO: #10 Parser TSV en JSON
     elif file.endswith('.json'):
-        logging.info(f"JSON file detected.")
+        print(f"JSON file detected: {file}")
+        logging.info(f"JSON file detected: {file}")
     else:
         content = f"Unsupported file format for file: {file}"
+        print(content)
         logging.error(content)
         send_mail_alert(recipients, content)
         return
     
     # Vérification du format JSON
+    print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("5. Si JSON : test du formattage du fichier")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
     if not check_json_format(file):
         content = "Failed to process JSON input file."
         logging.error(content)
@@ -171,14 +196,25 @@ def diagho_process_file(file, config):
         return
     
     # Extraction des informations de fichier
+    print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("6. Récupérer les infos du fichier JSON 'files'")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
     dict_files = get_files_infos(file)
+    pretty_print_json_string(dict_files)
     
     # Foreach filename : vérifier s'il existe dans le répertoire path_biofiles
+    print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("7. Pour chaque filename...")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
     for filename, file_info in dict_files.items():
         biofile = os.path.join(path_biofiles, filename)
         checksum = file_info["checksum"]
         persons = file_info["persons"]
         
+        print(f"filename           : {filename}")
+        print(f"Processing biofile : {biofile}")
+        print(f"Persons            : {persons}")
+
         logging.info(f"Processing biofile: {biofile}")
 
         # Tant que le biofile n'existe pas...
@@ -197,13 +233,25 @@ def diagho_process_file(file, config):
             send_mail_alert(recipients, content)
             continue
         
+        print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        print("8. Biofile trouvé")
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+        print(f"Biofile {biofile} found.")
         logging.info(f"Biofile {biofile} found.")
         
         # Récupérer le checksum du biofile
+        print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        print("8. Calcul du checksum")
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
         checksum_current_biofile = md5(biofile)
+        
+        print(f"Checksum : {checksum_current_biofile}")
         
         # Si c'est un VCF :
         if biofile.endswith('.vcf') or biofile.endswith('.vcf.gz'):
+            print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+            print("9. Process VCF")
+            print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
             process_vcf_file(biofile, checksum_current_biofile, config, recipients, file, persons)
             
         # Si c'est un BED :
@@ -233,23 +281,39 @@ def process_vcf_file(biofile, checksum_current_biofile, config, recipients, file
     Returns:
         None
     """
+    print(f"\nStart function : process_vcf\n")
+    
     url_diagho_api_biofiles = config['diagho_api']['biofiles']
+    url_diagho_api_loading_status = config['diagho_api']['loading_status']
     accession_id = config['accessions']['vcf']
-    checksum_from_api = diagho_api_post_biofile(url_diagho_api_biofiles, biofile, accession_id).get('checksum')
+    
+    # Post du biofile + récupérer le chekcsum
+    print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("10. POST du Biofile")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+
+    checksum_from_api = diagho_api_post_biofile(url_diagho_api_biofiles, biofile, accession_id, config).get('checksum')
+    print(f"Checksum renvoyé par l'API : {checksum_from_api}")
     
     # Si l'upload ne fonctionne pas : alerte
-    if not checksum_from_api:
+    if not checksum_from_api or checksum_from_api is None:
         content = "Failed to upload biofile and obtain checksum from API."
         logging.error(content)
+        print(content)
         send_mail_alert(recipients, content)
         return
     
     # Vérification du checksum du fichier uploadé avec le checksum calculé avant
     # Si OK : continuer
     if check_md5sum(checksum_from_api, checksum_current_biofile):
+        print(f"Les checksum sont identiques")
+        
+        print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        print("11. Check du Loading Status")
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
         max_retries = config['check_loading']['max_retries']
         delay = config['check_loading']['delay']
-        loading_status = check_loading_status(url_diagho_api_biofiles, checksum_from_api, max_retries, delay)
+        loading_status = check_loading_status(config, url_diagho_api_loading_status, checksum_current_biofile, max_retries, delay)
         
         if loading_status:
             content = "Loading File = SUCCESS"
@@ -266,15 +330,15 @@ def process_vcf_file(biofile, checksum_current_biofile, config, recipients, file
             logging.info(f"Found interpretation file: {interp_file}")
             
             if family_file:
-                diagho_api_post_config(config['diagho_api']['config'], family_file)
+                diagho_api_post_config(config['diagho_api']['config'], family_file, config)
                 time.sleep(10)
                 copy_and_remove_file(family_file, config['backup_data_files'])
             if files_file:
-                diagho_api_post_config(config['diagho_api']['config'], files_file)
+                diagho_api_post_config(config['diagho_api']['config'], files_file, config)
                 time.sleep(10)
                 # copy_and_remove_file(files_file, config['backup_data_files'])
             if interp_file:
-                diagho_api_post_config(config['diagho_api']['config'], interp_file)
+                diagho_api_post_config(config['diagho_api']['config'], interp_file, config)
                 time.sleep(10)
                 copy_and_remove_file(interp_file, config['backup_data_files'])
             
@@ -328,7 +392,7 @@ def process_bed_file(biofile, checksum_current_biofile, config, recipients, file
         # Check loading status
         max_retries = config['check_loading']['max_retries']
         delay = config['check_loading']['delay']
-        loading_status = check_loading_status(url_diagho_api_biofiles, checksum_from_api, max_retries, delay)
+        loading_status = check_loading_status(config, url_diagho_api_biofiles, checksum_from_api, max_retries, delay)
         
         if loading_status:
             # If loading status is successful
@@ -365,7 +429,7 @@ def process_bed_file(biofile, checksum_current_biofile, config, recipients, file
 
 
 # Check loading status
-def check_loading_status(url, checksum, max_retries, delay, attempt=0):
+def check_loading_status(config, url, checksum, max_retries, delay, attempt=0):
     """
     Vérification du statut de chargement.
 
@@ -374,6 +438,7 @@ def check_loading_status(url, checksum, max_retries, delay, attempt=0):
         max_retries: nombre de tentatives max.
         delay: délai (en secondes) entre chaque tentative.
         attempt: compteur de tentatives actuelles.
+        config: fichierd e conf
 
     Returns:
         True : si le fichier est chargé (status 3)
@@ -383,8 +448,7 @@ def check_loading_status(url, checksum, max_retries, delay, attempt=0):
     print("check_loading_status")
     print("MAX_RETRIES:", max_retries)
     # Get loading status :
-    ## TODO #6 mettre l'URL à partir du fichier de config
-    status = diagho_api_get_loadingstatus(url, checksum).get('loading')
+    status = diagho_api_get_loadingstatus(url, checksum, config).get('loading')
     print("Initial status=", status)
     
     while status not in [0, 3]:
@@ -396,7 +460,7 @@ def check_loading_status(url, checksum, max_retries, delay, attempt=0):
         time.sleep(delay)
         
         # Récupérer à nouveau le statut de chargement
-        status = diagho_api_get_loadingstatus(url, checksum).get('loading')
+        status = diagho_api_get_loadingstatus(url, checksum, config).get('loading')
         print("Nouveau statut =", status)
         
         # Incrémenter le compteur de tentatives
