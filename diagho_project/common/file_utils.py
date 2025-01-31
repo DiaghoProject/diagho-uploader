6c2f357bd7d45a6e4af479ecd0ca0d404e74189a
bnouyou <benedicte.nouyou@gmail.com>
2025-01-31 16:26:45 +0100

    add file_utils.py

---
+++ diagho_project/common/file_utils.py
@@ -0,0 +1,17 @@
+import json
+
+# Pretty print json string
+def pretty_print_json_string(string):
+    """
+    Pretty print a JSON string.
+
+    Arguments:
+        json_str : str : The JSON string to be pretty printed.
+    """
+    try:
+        json_string = json.dumps(string)
+        json_dict = json.loads(json_string)
+        print(json.dumps(json_dict, indent = 1))
+    except json.JSONDecodeError as e:
+        print(f"Invalid JSON string: {e}")
+     
