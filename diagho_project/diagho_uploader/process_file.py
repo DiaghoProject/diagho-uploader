aca8613ad9488c110557eb4a9ed9efff87e543ac
bnouyou <benedicte.nouyou@gmail.com>
2025-01-31 16:27:19 +0100

    Add JSON input validation

--- diagho_project/diagho_uploader/process_file.py
+++ diagho_project/diagho_uploader/process_file.py
@@ -7,7 +7,9 @@ from datetime import datetime
 import logging
 
 from .api_handler import *
-from .config_loader import *
+from common.config_loader import *
+from common.file_utils import *
+from common.mail_utils import *
 
 
 def diagho_upload_file(**kwargs):
@@ -36,11 +38,14 @@ def diagho_upload_file(**kwargs):
     
     
     # Test fichier JSON OK : missing key etc...
-    
-        # Si KO = envoi alerte par mail
-    
-    
-    
+    try:
+        dict_biofiles = validate_json_input(file_path)
+        print("Fichier JSON valide, traitement possible.")
+    except ValueError as e:
+        logging.getLogger("VALIDATE_JSON_INPUT").error(f"Erreur de validation du fichier JSON: {e}")
+        send_mail_alert(settings["recipients"], f"Erreur de validation du fichier JSON: {file_path}\n\n{e}")
+        print(f"Erreur de validation : {e}")
+        
     
     
     
@@ -68,7 +73,35 @@ def diagho_upload_file(**kwargs):
         
     
     
-    
-    
-    
-     
+
+def validate_json_input(json_input):
+    """
+    Valide la structure du fichier JSON.
+    """
+    try:
+        with open(json_input, 'r') as json_file:
+            input_data = json.load(json_file)
+
+        if "files" not in input_data:
+            
+            raise ValueError("Le fichier JSON doit contenir une clé 'files'.")
+
+        required_keys = ["filename", "checksum", "assembly", "samples"]
+        
+        for file in input_data["files"]:
+            # Vérifier la présence des clés obligatoires
+            missing_keys = [key for key in required_keys if key not in file]
+            if missing_keys:
+                raise ValueError(f"Clés manquantes: {missing_keys}")
+
+            # Vérifier la structure des samples
+            if not isinstance(file["samples"], list) or not file["samples"]:
+                raise ValueError(f"'samples' doit être une liste non vide pour '{file['filename']}'.")
+
+            for sample in file["samples"]:
+                if "person" not in sample:
+                    raise ValueError(f"Clé 'person' manquante dans un sample du fichier '{file['filename']}'.")
+        return input_data
+
+    except (json.JSONDecodeError, FileNotFoundError) as e:
+        raise ValueError(f"Erreur lors de la lecture du fichier JSON : {e}")
