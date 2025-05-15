29570afed1e6cf34b0d835f09e978aa81a762e96
Benedicte NOUYOU <benedicte.nouyou@gmail.com>
2025-05-15 16:12:47 +0200

    Change stop_uploader.sh (use PID)

--- stop_uploader.sh
+++ stop_uploader.sh
@@ -7,4 +7,26 @@ echo "
 
 "
 
+FORCE=$1
+
+# Arrêt du watcher
 touch stop_watcher.flag
+
+# Forcer l'arrêt de l'uploader (si en cours d'upload)
+if [[ "${FORCE:-}" == "force" ]]; then 
+    echo "Force stop uploader"
+    
+    CMD="python main.py start_file_watcher"
+    PID=$(pgrep -f "$CMD")
+    if [[ -n "$PID" ]]; then
+        echo "Process found with PID: $PID"
+        echo "Kill process..."
+
+        kill -9 $PID
+
+        echo "Process $PID terminated."
+
+    else
+        echo "No process found for: $CMD"
+    fi
+fi
